6341a229ee4dc02f2e1b69a2499beeae48022255
GNAGONI <nikichetotam@gmail.com>
2020-12-27 12:49:20 -0800

    Implement error handling in isOnline job

--- src/jobs/isOnlineJob.js
+++ src/jobs/isOnlineJob.js
@@ -4,21 +4,29 @@ const { getAllSessions } = require('../sessionStorage');
 
 const isOnlineJob = () => {
   cron.schedule(process.env.IS_ONLINE_JOB_SCHEDULE, async () => {
-    const sessions = await getAllSessions();
-    const publisher = new EventBusPublisher(
-      process.env.RABBITMQ_URI,
-      'isOnline',
-    );
-    await publisher.init();
-    sessions.forEach(session => {
-      const { userEmail, userId } = JSON.parse(session);
-      if (userEmail && userId) {
-        publisher.publish({
-          userEmail,
-          userId,
-        });
-      }
-    });
+    try {
+      const sessions = await getAllSessions();
+      const publisher = new EventBusPublisher(
+        process.env.RABBITMQ_URI,
+        'isOnline',
+      );
+      await publisher.init();
+      sessions.forEach(session => {
+        try {
+          const { userEmail, userId } = JSON.parse(session);
+          if (userEmail && userId) {
+            publisher.publish({
+              userEmail,
+              userId,
+            });
+          }
+        } catch (e) {
+          console.error('Invalid session: ', e);
+        }
+      });
+    } catch (e) {
+      console.error('Unexpected error: ', e);
+    }
   });
 };
 
