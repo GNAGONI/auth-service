2a03b1e7db954407ee8c9b9331fc4e719f9caad2
GNAGONI <nikichetotam@gmail.com>
2020-12-29 14:53:24 -0800

    Add flag isOnline to event

--- src/jobs/isOnlineJob.js
+++ src/jobs/isOnlineJob.js
@@ -14,6 +14,7 @@ const isOnlineJob = () => {
             publisher.publish({
               userEmail,
               userId,
+              isOnline: true,
             });
           }
         } catch (e) {
